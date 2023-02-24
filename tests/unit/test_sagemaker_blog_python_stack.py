a8bc1e550a496e4bf5e1a516a7e921e97ff1c12a
Yariv Levy <yalevy@amazon.com>
2023-02-24 12:57:09 +0200

    Reformatting and renaming

--- tests/unit/test_sagemaker_blog_python_stack.py
+++ tests/unit/test_sagemaker_blog_python_stack.py
@@ -1,14 +1,16 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from sagemaker_blog_python.sagemaker_blog_python_stack import SagemakerBlogPythonStack
+from sagemaker_gptj_stack.sagemaker_gptj_stack import SagemakerGPTJStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in sagemaker_blog_python/sagemaker_blog_python_stack.py
+# resource in sagemaker_gptj_stack/sagemaker_gptj_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = SagemakerBlogPythonStack(app, "sagemaker-blog-python")
+    stack = SagemakerGPTJStack(app, "sagemaker-gptj-stack")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
